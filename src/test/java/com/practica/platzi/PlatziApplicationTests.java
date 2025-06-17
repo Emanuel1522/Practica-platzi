ac89ff31cbb09a7f486ecb4265836f7da749935f
Emanuel1522 <emanuel.rr1522@gmail.com>
2025-06-16 20:50:10 -0500

    Configurando entorno de desarrollo

---
+++ src/test/java/com/practica/platzi/PlatziApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practica.platzi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatziApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
